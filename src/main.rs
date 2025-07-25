f1db95fdeb7eed69eb68de09620e753a8c8114af
apurbapokharel <pokharel.apurba@gmail.com>
2025-07-24 20:53:22 -0500

    Decode bencode integer

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,41 @@
+use serde_bencode::value;
 use serde_json;
-use std::env;
-
-// Available if you need it!
-// use serde_bencode
-
-// #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<usize>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
-}
+use std::{collections::HashMap, env};
 
 // Usage: your_program.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
-
+    let list = 'l';
+    let dict = 'd';
+    //string have 4:home
+    //numbers have i-3e
+    //list [a,b] = l1:a1:be
+    //dictionary {'cow': 'moo', 'spam': 'eggs'} = d3:cow3:moo4:spam4:eggse 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
         eprintln!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        match &encoded_value.chars().next() {
+            Some(value) => {
+                if value.is_digit(10){
+                let decoded_value : String = serde_bencode::from_str(&encoded_value).unwrap();
+                println!("{}", decoded_value);
+                }
+                else if value.eq(&list) {
+                    let decoded_value : Vec<String> = serde_bencode::from_str(&encoded_value).unwrap();
+                    println!("{:?}", decoded_value);
+                } 
+                // else if value.eq(&dict) {
+                //     let decoded_value : HashMap<> = serde_bencode::from_str(&encoded_value).unwrap();
+                //     println!("{:?}", decoded_value);
+                // }
+                else{
+                    let decoded_value : i64 = serde_bencode::from_str(&encoded_value).unwrap();
+                    println!("{}", decoded_value);
+                }
+            },
+            _ => panic!("Cannot decode: empty input string")
+        } 
     } else {
         println!("unknown command: {}", args[1])
     }
