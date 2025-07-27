e27bcd2e1a6ccd92f8bf0914ef7ff49148694a97
apurbapokharel <pokharel.apurba@gmail.com>
2025-07-26 22:21:45 -0500

    Trigger build list

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,67 @@
 #![allow(unused_imports)]
+use serde::de;
 use serde_bencode::value;
-use serde_json;
+use serde_json::{self, Number, Value};
 use std::{collections::HashMap, env};
+
+
+
+fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
+    match &encoded_value.chars().next() {
+        Some('i') => {
+            // if let Some(rest) = encoded_value.strip_prefix('i'){
+            //     if let Some((digits, remaining)) = rest.split_once('e'){
+            //         if let Ok(n) = digits.parse::<i64>(){
+            //             return (n.into(), remaining);
+            //         }
+            //     }
+            // }
+            if let Some((digit, rest)) = 
+                encoded_value.
+                    split_at(1)
+                    .1
+                    .split_once('e')
+                    .and_then(|(digits,rest)|{
+                        let n = digits.parse::<i64>().ok()?;
+                        return Some((n, rest))
+                    }){
+                        return (digit.into(), rest);
+                    }
+        }
+        Some('l') => {
+            let mut res = Vec::new();
+            let mut rem  = encoded_value.split_at(1).1;
+            while let Some(c) = rem.chars().next() {
+                if c == 'e' {
+                    break;
+                }
+                let (result, rest) = decode_bencoded_value(rem);
+                res.push(result);
+                rem = rest;
+            }
+            return (res.into(), &rem[1..])
+
+        }
+        // Some('d') => {
+
+        // }
+        Some('0'..='9') => {
+            if let Some((len, rest)) = encoded_value.split_once(':'){
+                if let Ok(len) = len.parse::<usize>() {
+                    return (serde_json::Value::String(rest[..len].to_string()), &rest[len..]);  
+                }
+            }
+        }
+        _ => {}
+    }    
+    panic!("Unhandled encoded value: {}", encoded_value);
+}
 
 // Usage: your_program.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
-    let list = 'l';
-    let dict = 'd';
+   
     //string have 4:home
     //numbers have i-3e
     //list [a,b] = l1:a1:be
@@ -16,27 +69,8 @@ fn main() {
     if command == "decode" {
         eprintln!("Logs from your program will appear here!");
         let encoded_value = &args[2];
-        match &encoded_value.chars().next() {
-            Some(value) => {
-                if value.is_digit(10){
-                let decoded_value : String = serde_bencode::from_str(&encoded_value).unwrap();
-                println!("{}", serde_json::Value::String(decoded_value));
-                }
-                else if value.eq(&list) {
-                    let decoded_value : Vec<String> = serde_bencode::from_str(&encoded_value).unwrap();
-                    println!("{:?}", decoded_value);
-                } 
-                // else if value.eq(&dict) {
-                //     let decoded_value : HashMap<> = serde_bencode::from_str(&encoded_value).unwrap();
-                //     println!("{:?}", decoded_value);
-                // }
-                else{
-                    let decoded_value : i64 = serde_bencode::from_str(&encoded_value).unwrap();
-                    println!("{}", decoded_value);
-                }
-            },
-            _ => panic!("Cannot decode: empty input string")
-        } 
+        let decoded_value = decode_bencoded_value(encoded_value).0;
+        println!("{decoded_value}");
     } else {
         println!("unknown command: {}", args[1])
     }
